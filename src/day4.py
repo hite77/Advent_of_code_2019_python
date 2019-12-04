eb7b45cd573add098ac99eaf5b1e49b0af6f692f
Jason Hite <jason.c.hite@accenture.com>
2019-12-04 15:44:40 -0500

    comparing for increasing or same

--- src/day4.py
+++ src/day4.py
@@ -9,4 +9,12 @@ def contains_at_least_one_duplicate(number):
             return True
     return False
 
+
+def numbers_increase_or_stay_same_from_left_to_right(number):
+    string_number = str(number)
+    for x in range(0, len(string_number)-1):
+        if int(string_number[x]) > int(string_number[x+1]):
+            return False
+    return True
+
 # range 178416-676461
--- test/test_day4.py
+++ test/test_day4.py
@@ -1,9 +1,7 @@
 import unittest
-from src.day4 import is_valid
+from src.day4 import is_valid, \
+    numbers_increase_or_stay_same_from_left_to_right
 
-# Going from left to right, the digits never decrease they increase or stay same
-# (Like 111123 or 135679)
-#
 # -111111 meets criteria (double 11, never decreases)
 # -223450 does not decreasing 5 to 0
 # -123789 no double
@@ -22,6 +20,12 @@ class MyTestCase(unittest.TestCase):
     def test_no_duplicate_digits_fails(self):
         self.assertEqual(False, is_valid(123456))
 
+    def test_checking_for_increase_or_same_for_a_number(self):
+        self.assertEqual(True, numbers_increase_or_stay_same_from_left_to_right(111123))
+        self.assertEqual(True, numbers_increase_or_stay_same_from_left_to_right(135679))
+        self.assertEqual(False, numbers_increase_or_stay_same_from_left_to_right(223450))
+
+# need to test for one of the above that is not decreasing for no duplicate to make sure it checks both
 
 if __name__ == '__main__':
     unittest.main()
